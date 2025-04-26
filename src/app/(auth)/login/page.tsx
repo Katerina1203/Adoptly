37e5d981474f528aedfbd41aebbd2546ec7633bc
Katerina1203 <katie_g@abv.bg>
2025-04-26 22:23:50 +0300

    Improvements to the login /user pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,42 +1,48 @@
-'use client';
-
-import { useState } from 'react';
+"use client"
+import React, { useState } from 'react';
 import LoginForm from '@/components/login/login';
 import RegisterForm from '@/components/login/register';
-import styles from './auth.module.css';
+import Image from 'next/image';
+import { handleGoogleLogin } from '@/lib/actions'
 
-export default function AuthPage() {
-  const [isLogin, setIsLogin] = useState(true);
+const FormSwitcher = () => {
+	const [isLogin, setIsLogin] = useState(true);
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.toggleButtons}>
-        <button
-          className={`${styles.toggleButton} ${isLogin ? styles.active : ''}`}
-          onClick={() => setIsLogin(true)}
-        >
-          Вход
-        </button>
-        <button
-          className={`${styles.toggleButton} ${!isLogin ? styles.active : ''}`}
-          onClick={() => setIsLogin(false)}
-        >
-          Регистрация
-        </button>
-      </div>
+	const toggleForm = () => {
+		setIsLogin(!isLogin);
+	};
 
-      <div className={styles.formContainer}>
-        {isLogin && (
-          <div className={styles.formWrapper}>
-            <LoginForm />
-          </div>
-        )}
-        {!isLogin && (
-          <div className={styles.formWrapper}>
-            <RegisterForm />
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="flex h-[75vh] w-full overflow-hidden">
+			<div className={`flex-1 hidden md:flex justify-center items-center relative transition-transform duration-500 ease-in-out  ${isLogin ? "md:translate-x-0" : "md:translate-x-full"}`}>
+				<Image
+					src={isLogin ? "/img/family-with-dog.jpg" : "/img/girl-with-cat.jpg"}
+					alt={isLogin ? "Family with dog" : "Girl with cat"}
+					fill
+					className="w-full h-full object-cover"
+				/>
+			</div>
+			<div className={`flex-1 flex flex-col justify-center items-center p-8 transition-transform duration-500 ease-in-out ${isLogin ? "md:translate-x-0" : "md:-translate-x-full"}`}>
+				{isLogin ? (
+					<div>
+						<LoginForm toggleForm={toggleForm} />
+
+						<form action={handleGoogleLogin} className="text-blue-500 text-ml">
+							<button>Вход с Google account</button>
+						</form>
+					</div>
+				) : (
+					<div>
+						<RegisterForm toggleForm={toggleForm} />
+						<form action={handleGoogleLogin} className="text-blue-500 text-ml">
+							<button>Регистрация с Google account</button>
+						</form>
+					</div>
+
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default FormSwitcher;
