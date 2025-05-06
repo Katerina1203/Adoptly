836a63703f877200bb5360a8fa4d947668111efe
Aleksandar Dimitrov <alex_dim01@abv.bg>
2025-05-06 16:20:27 +0300

    update test configurations

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,23 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files
   dir: './',
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/app/(.*)$': '<rootDir>/src/app/$1',
     '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
     '^@/(.*)$': '<rootDir>/src/$1',
-    // Handle CSS modules
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
   },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/playwright/'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/',
+    '<rootDir>/e2e/'],
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
   },
   transformIgnorePatterns: [
     '/node_modules/',
@@ -37,5 +33,4 @@ const customJestConfig = {
   ],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config
 module.exports = createJestConfig(customJestConfig);
